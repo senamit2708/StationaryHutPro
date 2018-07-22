0ed4d4f70d1c04a82ef89c2c08eb85ad423d1131
senamit2708 <senamit2708@gmail.com>
2018-07-22 22:10:57 +0530

    Added the project to firebase, removed androidx

--- app/src/main/java/com/example/senamit/stationaryhutpro/MainActivity.kt
+++ app/src/main/java/com/example/senamit/stationaryhutpro/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.senamit.stationaryhutpro
 
-import androidx.appcompat.app.AppCompatActivity
+import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
